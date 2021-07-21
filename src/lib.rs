7289ac26b6d37be25278908c580ee62779c21f3d
Mathias Lorenz <mathias.lorenz@netlight.com>
2021-07-21 21:58:24 +0200

    Ran clippy and fmt. Should probably have these in git hooks

--- src/lib.rs
+++ src/lib.rs
@@ -3,41 +3,44 @@ pub struct Hangman {
 }
 
 struct LetterStatus {
-  letter: char,
-  status: GuessedStatus
+    letter: char,
+    status: GuessedStatus,
 }
 
 enum GuessedStatus {
-  Guessed,
-  NotGuessed
+    Guessed,
+    NotGuessed,
 }
 
 impl Hangman {
-  pub fn new(word: String) -> Self {
-    let w: Vec<_> = word.chars().map(|c| LetterStatus {
-      letter: c,
-      status: GuessedStatus::NotGuessed
-    }).collect();
+    pub fn new(word: String) -> Self {
+        let w: Vec<_> = word
+            .chars()
+            .map(|c| LetterStatus {
+                letter: c,
+                status: GuessedStatus::NotGuessed,
+            })
+            .collect();
 
-    Hangman {
-      word: w
-    }
-  }
-
-  pub fn print_word(self) {
-    for l in self.word.iter() {
-      match l.status {
-        GuessedStatus::Guessed => println!("{}", format!("Guessed, letter is: {}", l.letter)),
-        GuessedStatus::NotGuessed => println!("{}", format!("Not guessed, letter is: {}", l.letter)),
-      }
+        Hangman { word: w }
     }
 
-    ()
-  }
+    pub fn print_word(self) {
+        for l in self.word.iter() {
+            match l.status {
+                GuessedStatus::Guessed => {
+                    println!("{}", format!("Guessed, letter is: {}", l.letter))
+                }
+                GuessedStatus::NotGuessed => {
+                    println!("{}", format!("Not guessed, letter is: {}", l.letter))
+                }
+            }
+        }
+    }
 }
 
 pub fn play_hangman(hangman: Hangman) {
-  println!("We are about to play hangman!");
-  hangman.print_word();
+    println!("We are about to play hangman!");
+    hangman.print_word();
     //println!("{}", format!("The word is: {}", hangman.word));
 }
