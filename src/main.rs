041da8f1225f847532c02917bf5f1888364eeac8
Mathias Lorenz <lorenz.mathias@gmail.com>
2021-11-02 22:28:23 +0100

    Add arguably crate to do cli argument passing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
+use arguably::ArgParser;
 use hangman_rs::*;
 
 fn main() {
+    let mut args_parser = ArgParser::new()
+        .helptext("Hangman! Please input word to guess in 'secret_word.txt' and pass number of guesses with option")
+        .version("0.1")
+        .option("num_guesses n", "4");
+
+    if let Err(err) = args_parser.parse() {
+        err.exit();
+    }
+
+    let num_guesses: u8 = args_parser
+        .value("num_guesses")
+        .parse()
+        .expect("Could not parse num_guesses from input");
+
     let secret_word = read_secret_word("secret_word.txt").unwrap();
-    let mut hangman = Hangman::new(&secret_word, 3);
+    let mut hangman = Hangman::new(&secret_word, num_guesses);
 
     hangman.play();
 }
